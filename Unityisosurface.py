72643c9387a387db4bbbabd77b7f8fb1a32f40be
Michael Vossen <mpvossen@uwm.edu>
2024-02-04 15:15:30 -0600

    Update Unityisosurface.py

--- Unityisosurface.py
+++ Unityisosurface.py
@@ -1,67 +1,103 @@
 '''
-M. Vossen
+M. P. Vossen
 Feburary, 2019
-Program that creates isosurfaces for unity to read
+Program that creates isosurfaces for the unity game engine to read and display
+
+M. P. Vossen
+Feburary, 2024
+Modernized and cleaned up the code
+
 '''
-
-
 import numpy as np 
 import netCDF4 as nc 
 import mcubes as mc  
 from metpy import calc 
-from metpy.units import units  
+from metpy.units import units
+from datetime import datetime
+import pandas as pd  
 
-#Path to file
+##########################
+# User Settings          #
+##########################
+
+#Path to file and file name
 filepath = ''
+
+#where the output unity asses is going to be save to
 saveFilepath = ''
 
+#the start and end dates you want isosurfaces for
+t_start = datetime(2014,2,19,0)
+t_end = datetime(2014,2,22,18)
 
-for days in ["19", "20", "21", "22"]:
-    for hours in ["00","06","12","18"]:
-        #filename
-        filename = "201402" + days + "_" + hours + "_pres_trimmed.nc"
+#how frequently you want data for.  This follows the convention
+#used by the pandas date_range funtion for frequency
+t_freq = "6H"
 
-        #Extracting data from netCDF4 data
-        data = nc.Dataset(filepath + filename)
+#indecies to use to slice out.  This is grabage and should
+#be changed to lat lon using xarray.  I don't have this file
+#type and I don't know how it is structured and so I can't fix
+#it right now
+latstart = 65
+latend = 140
+lonstart = 460
+lonend = 600
 
-        #slice out the domain needed
-        latstart = 65
-        latend = 140
-        lonstart = 460
-        lonend = 600
+#the first theta level you want in kelvin
+lowest_theta = 280
+#the last theta level you want in kelvin
+highest_theta = 340
+#the resolution of theta level you want in between the lowest
+#and highest theta level in kelvin
+theta_resolution = 5
 
-        #extract temp varable for teh theta calculation
-        temperature = data.variables['TMP_P0_L100_GLL0'][:,latstart:latend, lonstart:lonend] * units.kelvin
-        pressurelevels = data.variables["lv_ISBL0"][:]
+###############################
+# End User Settings           #
+###############################
 
-        #create bland pressure array of ones
-        pressure = np.ones(np.shape(temperature))
+dates = pd.date_range(start,stop, freq=freq)
 
-        #initalize the counter to keep track of the pressur eindex of the array
-        count = 0
+for day in dates:
+    #the date_range function outputs numpy dates, regular datetimes
+    #are easier to work with and so we convert
+    day = pd.to_datetime(day)
 
-        #creating an array of pressure for each pressure level
-        #NEEDED FOR METPY CALCULATION
-        for p in pressurelevels:
-            #go to pressure index and set it equal to the pressure
-            pressure[count,:,:] = pressure[count,:,:] * p
-            #go up an index
-            count += 1
-        
-        #give unts for pressure.  METPY needs this
-        pressure = pressure * units.pascals
+    #filename
+    filename = f"{day:%Y%m%d_%H}_pres_trimmed.nc"
 
-        #calculate theta
-        theta = calc.potential_temperature(pressure, temperature)
+    #Extracting data from netCDF4 data
+    data = nc.Dataset(filepath + filename)
 
-        #get rid of theta units
-        theta = theta * (1/units.kelvin)
 
-        for Theta in range(280,345,5):
 
-            #does something for the isosurface. (varable to find isosurface of level)
-            vertices, triangles = mc.marchin_cubes(np.array(theta), Theta)
+    #extract temp varable for teh theta calculation
+    temperature = data.variables['TMP_P0_L100_GLL0'][:,latstart:latend, lonstart:lonend] * units.kelvin
+    pressurelevels = data.variables["lv_ISBL0"][:]
 
-            #export results as a dae file
-            mc.export_mesh(vertices, triangles, saveFilepath + days + "thetaSurfaces/" + hours + "zSurfaces/" + hours + "z_" + days + "_" + str(Theta) + "Theta.dae", str(Theta) + "Surface")
-            print("Finished " + days + "_" + hours + "z_" + str(Theta) + "Surface")
+    #create bland pressure array of ones
+    pressure = np.ones(np.shape(temperature))
+
+    #creating an array of pressure for each pressure level
+    #NEEDED FOR METPY CALCULATION
+    for count, p in enumerate(pressurelevels):
+        #go to pressure index and set it equal to the pressure
+        pressure[count,:,:] = pressure[count,:,:] * p
+
+    
+    #give unts for pressure.  METPY needs this
+    pressure = pressure * units.pascals
+
+    #calculate theta
+    theta = calc.potential_temperature(pressure, temperature)
+
+    #get rid of theta units
+    theta = np.array(theta)
+
+    for Theta in range(lowest_theta,highest_theta+theta_resolution,theta_resolution):
+
+        #does something for the isosurface. (varable to find isosurface of level)
+        vertices, triangles = mc.marchin_cubes(theta, Theta)
+
+        #export results as a dae file
+        mc.export_mesh(vertices, triangles, f"{saveFilepath}{day:%d}thetaSurfaces/{day:%H}zSurfaces/{day:%H}z_{day:%d}_{str(Theta)}Theta.dae", f"{str(Theta)}Surface")
+        print(f"Finished {day:%m/%d/%Y %H%M} UTC {str(Theta)}K Surface")
